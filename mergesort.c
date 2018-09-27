ff819f6c961c80931ce526853d4070c69dd6d920
Varun Ravichandran <Vron16@users.noreply.github.com>
2018-09-27 00:49:19 -0400

    Completed much of body of merge function in mergesort.c in addition to the comparator functions and their prototypes in the header file. TODO: handle null cases

--- mergesort.c
+++ mergesort.c
@@ -2,26 +2,27 @@
 #include<stdlib.h>
 #include<string.h>
 
-void mergesort (Record records[], int left, int right, void *compare) {
+void mergesort (Record records[], int left, int right, int *compareFcnPtr(void*, void*)) {
 	//as long as mergesort is working on a valid subsection of the array of records (left pointer is less than the right pointer)
 	if (left < right) {
 		int mid = (left + right)/2; //calculate middle index of the array of records
-		mergesort(records, left, mid, *compare); //recursively sort the left half of the array of records
-		mergesort(records, mid + 1, right, *compare); //recursively sort the right of the array of records
-		merge(records, left, mid, right, *compare); //merge the two halves of the array by using the merge helper function
+		mergesort(records, left, mid, compareFcnPtr); //recursively sort the left half of the array of records
+		mergesort(records, mid + 1, right, compareFcnPtr); //recursively sort the right of the array of records
+		merge(records, left, mid, right, compareFcnPtr); //merge the two halves of the array by using the merge helper function
 	}
 }
 
 
-void merge (Record records[], int left, int mid, int right, void *compare) {
+void merge (Record records[], int left, int mid, int right, int *compareFcnPtr(void*, void*)) {
 	int leftSize = mid - left + 1;
 	int rightSize right - mid;
 	Record leftHalf[leftSize];
 	Record rightHalf[rightSize];
-	for (int i = 0; i < leftSize; i++) {
+	int i;
+	for (i = 0; i < leftSize; i++) {
 		leftHalf[i] = records[left + i];
 	}
-	for (int i = 0; i < rightSize; i++) {
+	for (i = 0; i < rightSize; i++) {
 		rightHalf[i] = records[mid + i + 1];
 	}
 	int leftIndex = 0;
@@ -31,7 +32,45 @@ void merge (Record records[], int left, int mid, int right, void *compare) {
 	while (leftIndex < leftSize && rightIndex < rightSize) {
 		Record leftTemp = leftHalf[leftIndex];
 		Record rightTemp = rightHalf[rightIndex];
+		int comparison = *compareFcnPtr(leftTemp->ptr, rightTemp->ptr);
+		if (comparison > 0) {
+			records[mergedIndex] = rightTemp;
+			mergedIndex++;
+			rightIndex++;
+		}
+		else if (comparison < 0) {
+			records[mergedIndex] = leftTemp;
+			mergedIndex++;
+			leftIndex++;
+		}
+		else {
+			records[mergedIndex] = leftTemp;
+			mergedIndex++;
+			leftIndex++;
+			records[mergedIndex] = rightTemp;
+			mergedIndex++;
+			rightIndex++;
+		}
 	}
 
+	while (leftIndex < leftSize) {
+		records[mergedIndex] = leftHalf[leftIndex];
+		mergedIndex++;
+		leftIndex++;
+	}
 
+	while (rightIndex < rightSize) {
+		records[mergedIndex] = rightHalf[rightIndex];
+		mergedIndex++;
+		rightIndex++;
+	}
+
+}
+//TODO: HANDLE NULL CASES
+int intComparator (void* data0, void* data1) {
+	return *((int*)data0) - *((int*)data1);
+}
+
+int strComparator (void* data0, void* data1) {
+	return strcmp(*((char*)data1), *((char*)data0));
 }
--- simpleCSVSorter.h
+++ simpleCSVSorter.h
@@ -10,20 +10,28 @@
 #define STDOUT 1
 #define STDERR 2
 
-typedef union valuePtr {
+/*typedef union valuePtr {
 	int *intKey;
 	char *charKey;
-} Key;
+} Key;*/
 typedef struct record {
-	Key ptr;
-	char *beginning;
-	char *end;
+	void *ptr; //void ptr to the data that mergesort.c utilizes to sort by (will be cast to int or char depending on the comparator function specified)
+	char *beginning; //char pointer to the start of the row of characters
+	char *end; //char pointer to the start of the remainder of the characters that occur after the data we are sorting on in the original row
 } Record;
 
 // Mergesort prototype headers for .c file to use
-// void* parameter will be the function pointer
+// int* parameter will be the function pointer
 // to the appropriate comparator function
-int mergesort(Record*, int, int, void*);
-int merge(Record*, int, int, int, void*);
+int mergesort(Record*, int, int, int*);
+int merge(Record*, int, int, int, int*);
+
+//intComparator and strComparator prototype headers for mergesort.c to use
+//Pointer to intComparator is passed into mergesort function for records containing numeric values
+//Pointer to strComparator is passed into mergesort function for records containing alphabetic values
+//Both functions take void* paramters for the two pieces of data being compared, which are then cast in the function bodies depending on
+//expected type. Allows for Record struct to contain one simplistic void*ptr that is cast as necessary when passed to the correct comparator function 
+int intComparator(void*, void*);
+int strComparator(void*, void*);
 
 #endif
